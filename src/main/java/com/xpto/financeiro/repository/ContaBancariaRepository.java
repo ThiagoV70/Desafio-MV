fe8f21b1c8eba89b44128333cdd73f9cc6354e0a
Thiago Santos <thiagoestuda9@gmail.com>
2025-10-25 20:02:29 -0300

    implements ContaBancariaRepository and EnderecoRepository in Repository package

---
+++ src/main/java/com/xpto/financeiro/repository/ContaBancariaRepository.java
@@ -0,0 +1,12 @@
+package com.xpto.financeiro.repository;
+
+import com.xpto.financeiro.model.ContaBancaria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ContaBancariaRepository extends JpaRepository<ContaBancaria, Long> {
+    List<ContaBancaria> findByClienteIdAndAtivoTrue(Long clienteId);
+}
